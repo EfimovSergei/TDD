d005a7b21310e4b6fc5bb088d10717a36f695d5c
Neto test <ehimovsergei@mail.ru>
2023-11-05 19:33:21 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class PhoneBook {
+    public int size() {
+        return 0;
+    }
+
+    public int add(String name, int number) {
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        int initialSize = phoneBook.size();
+        int newSize = phoneBook.add("John Doe", 1234567890);
+
+        Assertions.assertEquals(initialSize + 1, newSize);
+    }
 }
