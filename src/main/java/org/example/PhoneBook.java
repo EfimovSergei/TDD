718b5776aaaf78a7082b947f7ecbd8b9443aaa19
Neto test <ehimovsergei@mail.ru>
2023-11-06 19:01:29 +0300

    Stub and tests for printAllNames

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,37 +1,44 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PhoneBook {
-    static Map<String,Integer> MAP= null;
+    static Map<String, Integer> MAP = null;
+
     public int size() {
-        if (MAP!=null){
+        if (MAP != null) {
             return MAP.size();
         }
         return 0;
     }
 
     public int add(String name, int number) {
-        if(MAP!=null){
+        if (MAP != null) {
             if (!MAP.containsKey(name)) {
-                MAP.put(name,number);
+                MAP.put(name, number);
             }
-        }else{
-            MAP=new HashMap<>();
-            MAP.put(name,number);
+        } else {
+            MAP = new HashMap<>();
+            MAP.put(name, number);
         }
         return size();
     }
-    public String findByNumber(int number){
 
-            for(Map.Entry entry: MAP.entrySet()) {
-                if (entry.getValue().equals(number)) return entry.getKey().toString();
-            }
-            return null;
+    public String findByNumber(int number) {
+
+        for (Map.Entry entry : MAP.entrySet()) {
+            if (entry.getValue().equals(number)) return entry.getKey().toString();
+        }
+        return null;
     }
 
     public int findByName(String name) {
         return MAP.get(name);
+    }
+
+    public List<String> printAllNames() {
+        return null;
     }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -3,6 +3,9 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 class PhoneBookTest {
     @Test
     public void testAdd() {
@@ -31,5 +34,19 @@ class PhoneBookTest {
         String name = "John Doe";
         int number = phoneBook.findByName(name);
         Assertions.assertEquals(1234567890, number);
+    }
+    @Test
+    public void printAllNames() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Evgenii", 123124512);
+        phoneBook.add("Dmitriy", 346787239);
+        phoneBook.add("Alina", 1254734223);
+        phoneBook.add("Dasha", 964515521);
+
+        List<String> result = phoneBook.printAllNames();
+
+        List<String> expected = Arrays.asList("Alina", "Dasha", "Dmitriy", "Evgenii");
+
+        Assertions.assertEquals(expected,result);
     }
 }
