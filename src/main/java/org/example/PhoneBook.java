e7cf390e0ebe6be3e0adeb29045978e2fe7c4feb
Neto test <ehimovsergei@mail.ru>
2023-11-06 15:45:21 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,11 +1,22 @@
 package org.example;
 
+import java.util.Map;
+
 public class PhoneBook {
+    static Map<String,Integer> MAP= null;
     public int size() {
+        if (MAP!=null){
+            return MAP.size();
+        }
         return 0;
     }
 
     public int add(String name, int number) {
-        return 0;
+        if(MAP!=null){
+            if (!MAP.containsKey(name)) {
+                MAP.put(name,number);
+            }
+        }
+        return size();
     }
 }
